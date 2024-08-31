b9b2c1b28153c3cef407a1ebd80bbfae5352fbd8
Rama <ramanaththakur1@gmail.com>
2024-08-31 18:24:03 +0530

    admin

--- hackinnoa04/src/app/components/ui/Home.tsx
+++ hackinnoa04/src/app/components/ui/Home.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import PatientForm from "@/app/components/ui/PatientForm";
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Homepage() {
   return (
-    <div className='flex items-center justify-center h-screen w-screen spa'>
+    <div className='flex overflow-x-hidden items-center justify-center max-h-screen h-screen'>
         <div className="left-sect space-y-3 ">
             <h2 className='text-xl'>AppName</h2>
             <PatientForm />
             <h6 className='text-gray-600 text-xs '>&copy Team Quantammania HI24A04</h6>
+            <p>
+            <Link href="/?admin=true" className="text-green-500">Admin</Link> 
+            </p>
         </div>
-        {/* <div className="right-sect">
-            <div className="imageToBeUsed border-2 h-[300px] w-[300px]">
-                <h5>Hey</h5>
+        <div className="right-sect">
+            {/* <div className="imageToBeUsed ">
+                <Image
+                 src='/hackinnoa04/src/app/components/img.jpg'
+                 layout='fill'
+                 alt='patient'
+                 className='side-img'
+                />
             </div>
-        </div> */}
+                <h5>Hey</h5> */}
+        </div>
     </div>
   )
 }
